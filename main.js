02a419b3cdf8f63c49e3213e89a0dfbc1469ba84
Beluccii <belenlagos@gmail.com>
2023-08-07 20:24:18 -0300

    path y metodos arreglados

--- main.js
+++ main.js
@@ -3,70 +3,71 @@ import {promises as fs} from "fs"
 
 class ProductManager{
     constructor (){
-        this.path= "./productos.txt"
-    }}
+        this.path = "./productos.txt"
+    }
 
 
 // Método Add Product
 
-    const addProduct = async (product) => {
-        const products = JSON.parse(await fs.readFile ("./productos.txt", "utf-8"))
-        
-        if (products.find(producto => producto.id === product.id)){
-            return console.log ("Producto ya agregado")
-        
-            products.push(product)  
-        }
-    
-        await fs.writeFile("./productos.txt", JSON.stringify(products))
-    }
+        async addProduct (product){
+            const dataFromFile = await fs.readFile (this.path, "utf-8")
+            const products = JSON.parse(dataFromFile)
 
+            if (products.find(producto => producto.id === product.id)){
+                return console.log ("Producto ya agregado")
+            } else {
+                products.push(product)  
+            }
+        
+            await fs.writeFile(this.path, JSON.stringify(products))
+
+        }
 
 // Método Get Product
 
-    const getProducts= async() =>{
-        const products= JSON.parse(await fs.readFile (this.path, "utf-8"))
-        console.log (products)
-    }
+        async getProducts () {
+            const products= JSON.parse(await fs.readFile (this.path, "utf-8"))
+            console.log (products)
+        }
 
 
 //Método Get product by ID
 
-    const getProductsByID= async(id)=>{
-        const products = this.path
-    const prod = this.path.find(producto => producto ===id)
-        if (prod){
-            console.log (prod)
-        } else {
-            console.log ("Not found")
+       async getProductsByID(id){
+            const prod = this.path.find(producto => producto ===id)
+            if (prod){
+                console.log (prod)
+            } else {
+                console.log ("Not found")
+            }
         }
-    }
 
-    
- // Método Update Product
-    const updateProduct = async (id, { title }) =>{
-    const products= JSON.parse(await fs.readFile ("./productos.txt", "utf-8"))
-    const indice = products.findIndex(prod => prod.id ===id)
 
-    if (indice != -1){
-        products [indice].title = title
-        await fs.writeFile ("./productos.txt", JSON.stringify(products))
-    } else {
-        console.log ("Producto no encontrado")
-    }
-}
+// Método Update Product
+    async updateProduct (id, { title }) {
+        const products= JSON.parse(await fs.readFile (this.path, "utf-8"))    
+        const indice = products.findIndex(prod => prod.id ===id)
+
+        if (indice != -1){
+            products [indice].title = title
+            await fs.writeFile (this.path, JSON.stringify(products))
+        } else {
+            console.log ("Producto no encontrado")
+        }
+        }
 
 // Método Delete Product
 
-    const deleteProduct = async (id) =>{
-    const products= JSON.parse(await fs.readFile ("./productos.txt", "utf-8"))
-    const prods = products.filter(prod => prod.id !=id)
-    await fs.writeFile("./productos.txt", JSON.stringify(prods))
-}
+    async deleteProduct(id) {
+        const products= JSON.parse(await fs.readFile (this.path, "utf-8"))
+        const prods = products.filter(prod => prod.id !=id)
+        await fs.writeFile("./productos.txt", JSON.stringify(prods))
+        }
 
-
-
-
+        
+        
+ }
+    
 class Product {
     constructor(title, description, price, thumbnail, code, stock) {
         this.title = title
@@ -86,6 +87,8 @@ class Product {
         }
         return this.idIncrement
     }
+
 }
 
+new Product ({title:"arroz", price: "20", id: "5" })
 
